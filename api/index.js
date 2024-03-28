56252cf49ac646f704b5d5adda351ce87d618fa3
Gulshan <gulshan741759@gmail.com>
2024-03-28 22:39:11 +0530

    create and test test api

--- api/index.js
+++ api/index.js
@@ -1,21 +1,25 @@
 import express from 'express';
+const app = express();
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from './routes/user.route.js';
 dotenv.config();
-
-  mongoose.connect('mongodb+srv://gulshan74:gulshan7417@nodejs.496dqfq.mongodb.net/mern-blog/?retryWrites=true&w=majority&appName=Nodejs').then(()=>{
-    console.log("MongoDb is Connected")
-  }).catch((err)=>{
-    console.log(err)
+mongoose
+  .connect(
+    'mongodb+srv://gulshan74:gulshan7417@nodejs.496dqfq.mongodb.net/mern-blog/?retryWrites=true&w=majority&appName=Nodejs'
+  )
+  .then(() => {
+    console.log('MongoDb is Connected');
   })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Server is running ');
-});
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+  
+  
+  
+  app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+    
+    app.use('/api/user', userRouter);

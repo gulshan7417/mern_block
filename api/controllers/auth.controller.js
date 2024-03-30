36969482978cb02d9b070eea338430d2938d6d1e
Gulshan <gulshan741759@gmail.com>
2024-03-30 16:01:16 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from '../models/user.modal.js';
 import bcrypt from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
+
+
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
 
@@ -28,3 +31,29 @@ export const signup = async (req, res,next) => {
 };
 
 
+export const signin=async (req,res,next)=>{
+  const {email,password}=req.body;
+  try {
+      if (!email || !password) {
+        next(errorHandler(400, 'please provide email and password both'));
+      }
+
+      const validUser = await User.findOne({ email });
+    if(!validUser){
+      return next(errorHandler(404,'User not found'));
+    }
+
+      const validPassword=bcrypt.compareSync(password,validUser.password);
+      if(!validPassword){
+      return next(errorHandler(401,'Wrong credential!'))
+      }
+
+      const token=jwt.sign({id:validUser._id},'gulshan');
+      const {password:Pass,...rest}=validUser._doc;
+      res.cookie('access_token',token,{httpOnly:true,expires:new Date(Date.now()+24*60*60*1000000)}).status(200).json(rest)
+
+
+  } catch (error) {
+    next(error)
+  }
+}

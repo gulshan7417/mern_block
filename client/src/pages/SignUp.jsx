74bec22618146894148decb8aa21596deb60aa52
Gulshan <gulshan741759@gmail.com>
2024-03-30 21:16:52 +0530

    add redux persist

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -48,7 +48,7 @@ const SignUp = () => {
             className="  text-sm sm:text-xl font-bold dark:text-white"
           >
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Sahands
+              SHARE
             </span>
             Blog
           </Link>
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,26 +1,29 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+// import OAuth from '../components/OAuth';
 
-const SignIn = () => {
-  const navigate = useNavigate();
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (!formData.email || !formData.password) {
-      setErrorMessage('please fill out all field');
-      return; // Exit the function early
+      return dispatch(signInFailure('Please fill all the fields'));
     }
-
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart);
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -28,98 +31,84 @@ const SignIn = () => {
       });
       const data = await res.json();
       if (data.success === false) {
-        setErrorMessage(data.message);
-      } else if (res.ok) {
+        dispatch(signInFailure(data.message));
+      }
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/home');
       }
-      setLoading(false);
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
+      
     }
   };
-
-
   return (
-    <div className="min-h-screen md:mt-48 mt-24 ">
-      <div className="flex flex-col md:flex-row p-3 max-w-[80%] mx-auto  md:items-center gap-6 md:gap-4">
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
         <div className="flex-1">
-          <Link
-            to="/ "
-            className="  text-sm sm:text-xl font-bold dark:text-white"
-          >
+          <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Sahans
+              SHARE
             </span>
             Blog
           </Link>
-          <p className="text-1xl mt-5 ">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam,
-            saepe nesciunt voluptates veritatis illo rerum
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign in with your email and password
+            or with Google.
           </p>
         </div>
+        {/* right */}
 
-        <div className="  flex-1 ">
-          <div className="flex justify-center items-center mb-5">
-            <h1 className="text-2xl font-bold self-center p-5 ">Sign In</h1>
-          </div>
-          <form
-            className=" flex flex-col justify-center "
-            onSubmit={handleSubmit}
-          >
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value="Your email" htmlFor="email"></Label>
+              <Label value="Your email" />
               <TextInput
                 type="email"
+                placeholder="name@company.com"
                 id="email"
-                placeholder="Enter Your email"
-                className="w-[90%]"
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value="Your Password" htmlFor="password"></Label>
+              <Label value="Your password" />
               <TextInput
                 type="password"
+                placeholder="**********"
                 id="password"
-                placeholder="Password"
-                className="w-[90%]"
                 onChange={handleChange}
               />
             </div>
-
-            <div className="flex  ">
-              <Button
-                gradientDuoTone="purpleToPink"
-                type="submit"
-                className="mt-5 self-center w-[90%]"
-                disabled={loading}
-              >
-                {loading ? (
-                  <>
-                    <Spinner size="sm" />
-                    <span className="pl-3">Loading...</span>
-                  </>
-                ) : (
-                  'Sign In'
-                )}
-              </Button>
-            </div>
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
           </form>
-          <div className="flex gap-2 text-sm mt-5  ">
-            <span> Don t Have an account?</span>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Dont Have an account?</span>
             <Link to="/signup" className="text-blue-500">
               Sign Up
             </Link>
           </div>
           {errorMessage && (
-            <Alert className="mt-5 text-[17px] w-[90%]" color="failure">
-              please fill out all the fields
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
             </Alert>
           )}
         </div>
       </div>
     </div>
   );
-};
-export default SignIn;
+}

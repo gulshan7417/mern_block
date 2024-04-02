2dd2a2a48ad3b613565f64bfb7b438117f0a752f
Gulshan <gulshan741759@gmail.com>
2024-04-03 00:49:31 +0530

    add showMore  functionality

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,9 +1,122 @@
-
-
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Button, Table } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 const DashPost = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        if (res.ok) {
+          const data = await res.json();
+          setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          } else {
+            setShowMore(true);
+          }
+        } else {
+          throw new Error('Failed to fetch posts');
+        }
+      } catch (error) {
+        // Handle errors here
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+  const handleShowMore=async ()=>{
+    const startIndex=userPosts.length;
+    try {
+          const res =await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+          const data=await res.json();
+          if(res.ok){
+            setUserPosts((prev)=>[...prev,...data.posts]);
+            if(data.posts.length<9){
+              setShowMore(false)
+            }
+
+          }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>DashPost</div>
-  )
-}
-export default DashPost
+    <>
+      <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-state-700 dark:scrollbar-thumb-slate-500">
+        {currentUser.isAdmin && userPosts.length > 0 ? (
+          <>
+            <Table hoverable className="shadow-md">
+              <Table.Head>
+                <Table.HeadCell>Date updated</Table.HeadCell>
+                <Table.HeadCell>Post image</Table.HeadCell>
+                <Table.HeadCell>Post title</Table.HeadCell>
+                <Table.HeadCell>Category</Table.HeadCell>
+                <Table.HeadCell>Delete</Table.HeadCell>
+                <Table.HeadCell>Edit</Table.HeadCell>
+              </Table.Head>
+              <Table.Body className="divide-y">
+                {userPosts.map((post) => (
+                  <Table.Row
+                    key={post._id}
+                    className="bg-white dark:border-gray-800"
+                  >
+                    <Table.Cell>
+                      {new Date(post.updatedAt).toLocaleDateString()}
+                    </Table.Cell>
+
+                    <Table.Cell>
+                      <Link to={`/post/${post.slug}`}>
+                        <img
+                          src={post.image}
+                          alt={post.title}
+                          className="w-20 h-10 object-cover bg-gray-500"
+                        />
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link className="font-medium text-gray-900 dark:text-white">
+                        {post.title}
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>{post.category}</Table.Cell>
+                    <Table.Cell>
+                      <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                        Delete
+                      </span>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link
+                        className="text-teal-500hover:underline"
+                        to={`/update-post/${post._id}`}
+                      >
+                        Edit
+                      </Link>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+            {showMore && (
+              <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">
+                show more
+              </button>
+            )}
+          </>
+        ) : (
+          <p>There is no post yet</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default DashPost;

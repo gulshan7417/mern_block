47da96c53f5d81ec9c0078a26d491e7f4023a4f1
Gulshan <gulshan741759@gmail.com>
2024-04-04 16:32:05 +0530

    add comment to the post page

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
-
+import commentRoute from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose
@@ -28,6 +28,7 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRoutes);
 app.use('/api/post',postRoutes)
+app.use('/api/comment',commentRoute)
 app.use( (err,req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,10 +12,12 @@ import Profile from './pages/Profile';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import Post from './pages/Post';
+import ScrollToTop from './components/ScrollToTop';
 
 const App = () => {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Headers />
       <Routes>
         <Route path="/" element={<Home />} />

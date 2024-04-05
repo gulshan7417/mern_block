8b2be30371b890db067e39e22ca1f00651ad6f38
Gulshan <gulshan741759@gmail.com>
2024-04-05 23:33:28 +0530

    add preinstallation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoute from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path'
 dotenv.config();
 mongoose
   .connect(
@@ -18,6 +19,10 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+   const __dirname=path.resolve();
+
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -28,7 +33,14 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRoutes);
 app.use('/api/post',postRoutes)
+
 app.use('/api/comment',commentRoute)
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 app.use( (err,req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
